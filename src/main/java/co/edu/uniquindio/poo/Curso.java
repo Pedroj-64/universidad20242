ec2070f5344e9a4bed596b2cee08a4e736cad964
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2024-08-28 15:58:24 -0500

    kl

--- src/main/java/co/edu/uniquindio/poo/Curso.java
+++ src/main/java/co/edu/uniquindio/poo/Curso.java
@@ -1,12 +1,35 @@
 package co.edu.uniquindio.poo;
 
+import java.util.Collection;
+import java.util.LinkedList;
+
 public class Curso {
     private String nombre;
+    private Profesor profesor;
     private int notaDef;
+    private Collection<Estudiante> estudiantes;
 
-    public Curso(String nombre, int notaDef) {
+    public Curso(String nombre, int notaDef, Profesor profesor) {
+        this.profesor = profesor;
         this.nombre = nombre;
         this.notaDef = notaDef;
+        estudiantes = new LinkedList<>();
+    }
+
+    public Profesor getProfesor() {
+        return profesor;
+    }
+
+    public void setProfesor(Profesor profesor) {
+        this.profesor = profesor;
+    }
+
+    public Collection<Estudiante> getEstudiantes() {
+        return estudiantes;
+    }
+
+    public void setEstudiantes(Collection<Estudiante> estudiantes) {
+        this.estudiantes = estudiantes;
     }
 
     public void setNombre(String nombre) {
