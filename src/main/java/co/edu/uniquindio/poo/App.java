8cc04e23d655d1f2f51ac57da4f795c9f180237a
Pedroj-64 <pedroj.sotor@uqvirtual.edu.co>
2024-08-29 14:29:24 -0500

    pi

--- src/main/java/co/edu/uniquindio/poo/App.java
+++ src/main/java/co/edu/uniquindio/poo/App.java
@@ -10,69 +10,73 @@ public class App {
      * @param args
      */
     public static void main(String[] args) {        
-        Estudiante pepito=creacionEstudiante();
-        Profesor esteban=creacionProfesor();
-        ProgramaAcademico programa=creacionDeProgramaAcademico();
-        Curso curso=creacionDeCurso(esteban);       
-         UniversidadDelQuindio universidad=creacionUnivesidad(programa, curso);
-        pepito.setNombre("Mateus");  //Metodo que cambia el nombre del estudiante antes de printearlo
-        String mensaje=generarMensaje(universidad, esteban, programa, curso, pepito);
+        Estudiante pepito = creacionEstudiante();
+        Profesor esteban = creacionProfesor();
+        ProgramaAcademico programa = creacionDeProgramaAcademico();
+        Curso curso = creacionDeCurso(esteban);       
+        UniversidadDelQuindio universidad = creacionUnivesidad(programa, curso);
+        pepito.setNombre("Mateus");  // Método que cambia el nombre del estudiante antes de imprimirlo
+        String mensaje = generarMensaje(universidad, esteban, programa, curso, pepito);
         mostrarMensaje(mensaje);
-
     }
 
 
     /**
-     * Metodo para la creacion del profesor a partir del constructor del mismo
+     * Método para la creación del profesor a partir del constructor del mismo
      * @return
      */
-    public static Profesor creacionProfesor(){
-        Profesor esteban=new Profesor("Esteban", "405689203", Categoria.AUXILIAR);
+    public static Profesor creacionProfesor() {
+        Profesor esteban = new Profesor("Esteban", "405689203", Categoria.AUXILIAR);
         return esteban;
     }
+
     /**
-     * Metodo para la creacion del curso a partir del constructor del mismo, utilizando el nombre del profesor asignado en este caso esteban ( por ello se getea al final)
+     * Método para la creación del curso a partir del constructor del mismo, utilizando el nombre del profesor asignado en este caso Esteban (por ello se obtiene al final)
      * @param esteban
      * @return
      */
-    public static Curso creacionDeCurso(Profesor esteban){
-        Curso curso1=new Curso("Programacion I", 5, esteban.getNombre());
+    public static Curso creacionDeCurso(Profesor esteban) {
+        Curso curso1 = new Curso("Programación I", 5, esteban.getNombre());
         return curso1;
     }
+
     /**
-     * Metodo para la creacion del Programa academico a partir del constructor del mismo
+     * Método para la creación del Programa académico a partir del constructor del mismo
      * @return
      */
-    public static ProgramaAcademico creacionDeProgramaAcademico(){
-        ProgramaAcademico programa=new ProgramaAcademico("ing de sistemas y computacion");
+    public static ProgramaAcademico creacionDeProgramaAcademico() {
+        ProgramaAcademico programa = new ProgramaAcademico("Ingeniería de Sistemas y Computación");
         return programa;
     }
+
     /**
-     * Metodo para la creacion de la universidad  a partir del constructor del mismo
+     * Método para la creación de la universidad a partir del constructor del mismo
      * @return
      */
-    public static UniversidadDelQuindio creacionUnivesidad(ProgramaAcademico programa,Curso curso){
-        UniversidadDelQuindio universidad=new UniversidadDelQuindio(programa.getNombre(), curso.getNombre());
+    public static UniversidadDelQuindio creacionUnivesidad(ProgramaAcademico programa, Curso curso) {
+        UniversidadDelQuindio universidad = new UniversidadDelQuindio(programa.getNombre(), curso.getNombre());
         return universidad;
     }
+
     /**
-     * Se crea un Objeto de tipo Estudiante con todos sus atributos
+     * Se crea un objeto de tipo Estudiante con todos sus atributos
      * @return
      */
     public static Estudiante creacionEstudiante() {
-        Estudiante pepito = new Estudiante("Pepito", "lopez", "109234", "58534721", "pepito@gmail.com" ,(byte) 4,7,8,5);
+        Estudiante pepito = new Estudiante("Pepito", "Lopez", "109234", "58534721", "pepito@gmail.com", (byte) 4, 7, 8, 5);
         return pepito;
     }
-    public static String generarMensaje(UniversidadDelQuindio uni,Profesor profesor,ProgramaAcademico pAcademico, Curso curso, Estudiante estudiante){
-        String mensaje=(uni.toString()+profesor.toString()+pAcademico.toString()+curso.toString()+estudiante.toString());
+
+    public static String generarMensaje(UniversidadDelQuindio uni, Profesor profesor, ProgramaAcademico pAcademico, Curso curso, Estudiante estudiante) {
+        String mensaje = uni.toString() + profesor.toString() + pAcademico.toString() + curso.toString() + estudiante.toString();
         return mensaje;
     }
+
     /**
-     * Metodo para mostrar el mensaje final
+     * Método para mostrar el mensaje final
      * @param mensaje
      */
-
-    public static void mostrarMensaje(String mensaje){
+    public static void mostrarMensaje(String mensaje) {
         System.out.println(mensaje);
     }
 }
